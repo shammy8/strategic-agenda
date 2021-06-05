03990bbc444c0a43f0adfa48fa4b6bce2ca04b4e
Allan Sham <allansham@hotmail.com>
2021-06-05 16:59:16 +0100

    Add more characters

--- libs/feature-characters/src/lib/character.service.ts
+++ libs/feature-characters/src/lib/character.service.ts
@@ -11,7 +11,7 @@ export class CharacterService {
       firstName: 'Jeff',
       lastName: 'Winger',
       email: 'jeff.winger@lawfirm.com',
-      quote: 'hi',
+      quote: 'I see your value now',
       age: 39,
     },
     {
@@ -19,7 +19,8 @@ export class CharacterService {
       firstName: 'Britta',
       lastName: 'Perry',
       email: 'saveTheDolphins@hotmail.com',
-      quote: 'britta',
+      quote:
+        "I know what an analogy is. It's like a thought with another thoughts hat on.",
       age: 27,
     },
     {
@@ -28,7 +29,39 @@ export class CharacterService {
       lastName: 'Nadir',
       email: 'cool@hotmail.com',
       quote: 'Cool cool cool',
-      age: 27,
+      age: 23,
+    },
+    {
+      id: 4,
+      firstName: 'Troy',
+      lastName: 'Barnes',
+      email: 'childish@gambino.com',
+      quote: 'Donde, está, la biblioteca. Me llamo T-Bone, la araña discoteca',
+      age: 22,
+    },
+    {
+      id: 5,
+      firstName: 'Annie',
+      lastName: 'Edison',
+      email: 'annieedison@communitycollege.com',
+      quote: 'A passing grade? Like a C?!',
+      age: 22,
+    },
+    {
+      id: 6,
+      firstName: 'Shirley',
+      lastName: 'Bennett',
+      email: 'shirshir@shirleysbakery.com',
+      quote: "That's nice",
+      age: 40,
+    },
+    {
+      id: 7,
+      firstName: 'Pierce',
+      lastName: 'Hawthorne',
+      email: 'pierce@hawthronewipes.com',
+      quote: 'How long was I out? Is Napster still a thing?',
+      age: 63,
     },
   ];
 
