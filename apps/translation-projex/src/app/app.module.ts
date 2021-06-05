1eee09ad0ac29b07d095b07069771e8600ca10a8
Allan Sham <allansham@hotmail.com>
2021-06-05 12:46:51 +0100

    Add primeng

--- apps/translation-projex/src/app/app.module.ts
+++ apps/translation-projex/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
