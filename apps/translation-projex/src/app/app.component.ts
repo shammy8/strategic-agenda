a24ecbc5d7a0cd9a685cd1fb8be3667dc8e480aa
Allan Sham <allansham@hotmail.com>
2021-06-05 13:30:55 +0100

    Add a header

--- apps/translation-projex/src/app/app.component.ts
+++ apps/translation-projex/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'strategic-agenda-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'translation-projex';
-}
+export class AppComponent {}
--- apps/translation-projex/src/app/app.module.ts
+++ apps/translation-projex/src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
+import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, ButtonModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    ButtonModule,
+    MenubarModule,
+    InputTextModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
