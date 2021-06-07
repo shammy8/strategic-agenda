6d9bd6eef1639afa7a61f449c61edf6a7fc716c9
Allan Sham <allansham@hotmail.com>
2021-06-07 20:22:50 +0100

    Move the list of characters to own file and add more characters

--- libs/feature-characters/src/lib/character.service.ts
+++ libs/feature-characters/src/lib/character.service.ts
@@ -1,72 +1,14 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Character } from './character.model';
+import { characters } from './characters';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharacterService {
-  characters: Character[] = [
-    {
-      id: 1,
-      firstName: 'Jeff',
-      lastName: 'Winger',
-      email: 'jeff.winger@lawfirm.com',
-      quote: 'I see your value now',
-      age: 39,
-    },
-    {
-      id: 2,
-      firstName: 'Britta',
-      lastName: 'Perry',
-      email: 'saveTheDolphins@hotmail.com',
-      quote:
-        "I know what an analogy is. It's like a thought with another thoughts hat on.",
-      age: 27,
-    },
-    {
-      id: 3,
-      firstName: 'Abed',
-      lastName: 'Nadir',
-      email: 'cool@hotmail.com',
-      quote: 'Cool cool cool',
-      age: 23,
-    },
-    {
-      id: 4,
-      firstName: 'Troy',
-      lastName: 'Barnes',
-      email: 'childish@gambino.com',
-      quote: 'Donde, está, la biblioteca. Me llamo T-Bone, la araña discoteca',
-      age: 22,
-    },
-    {
-      id: 5,
-      firstName: 'Annie',
-      lastName: 'Edison',
-      email: 'annieedison@communitycollege.com',
-      quote: 'A passing grade? Like a C?!',
-      age: 22,
-    },
-    {
-      id: 6,
-      firstName: 'Shirley',
-      lastName: 'Bennett',
-      email: 'shirshir@shirleysbakery.com',
-      quote: "That's nice",
-      age: 40,
-    },
-    {
-      id: 7,
-      firstName: 'Pierce',
-      lastName: 'Hawthorne',
-      email: 'pierce@hawthronewipes.com',
-      quote: 'How long was I out? Is Napster still a thing?',
-      age: 63,
-    },
-  ];
-
-  private charactersSubject$ = new BehaviorSubject(this.characters);
+  characters: Character[] = characters;
+  private charactersSubject$ = new BehaviorSubject(characters);
   charactersObservable$ = this.charactersSubject$.asObservable();
 
   addCharacter(newCharacter: Character) {
